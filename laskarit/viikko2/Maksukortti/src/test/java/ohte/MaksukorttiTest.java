7dd2e9217e440d313d18f0666dcb88c78b17aadd
Mitja Karhusaari <mitja@karhusaari.me>
2020-11-07 23:09:46 +0200

    Create Maksupaate project and write tests according to the course JUnit guide

---
+++ laskarit/viikko2/Maksukortti/src/test/java/ohte/MaksukorttiTest.java
@@ -0,0 +1,54 @@
+package ohte;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MaksukorttiTest {
+    Maksukortti kortti;
+
+    public MaksukorttiTest() {
+    }
+
+    @Before
+    public void alustaMaksukortti() {
+        kortti = new Maksukortti(10);
+    }
+
+    @Test
+    public void kunstruktoriAsettaaSaldonOikein() {
+      String vastaus = kortti.toString();
+      assertEquals("Kortilla on rahaa 10.0 euroa", vastaus);
+    }
+
+    @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+      kortti.syoEdullisesti();
+      assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoMaukkaastiVahentaaSaldoaOikein() {
+        kortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 6.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 2.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(25);
+        assertEquals("Kortilla on rahaa 35.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortinSaldoEiYlitaMaksimiarvoa() {
+        kortti.lataaRahaa(200);
+        assertEquals("Kortilla on rahaa 150.0 euroa", kortti.toString());
+    }
+}
