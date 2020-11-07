d34f098173adebcf3033c6d06084c0a3d9029a54
Mitja Karhusaari <mitja@karhusaari.me>
2020-11-07 23:29:14 +0200

    Write tests for Maksukortti.

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -8,6 +8,10 @@ public class MaksukorttiTest {
 
     Maksukortti kortti;
 
+    private static void assertSaldo(int saldo, Maksukortti kortti) {
+      assertEquals("saldo: " + (saldo/100) + "." + (saldo%100), kortti.toString());
+    }
+
     @Before
     public void setUp() {
         kortti = new Maksukortti(10);
@@ -16,5 +20,38 @@ public class MaksukorttiTest {
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+
+    @Test
+    public void luodullaKortillaOnOikeaSaldo() {
+      assertSaldo(10, kortti);
+    }
+
+    @Test
+    public void rahanLataaminenKasvattaaSaldoa() {
+      kortti.lataaRahaa(100);
+      assertSaldo(110, kortti);
+    }
+
+    @Test
+    public void saldoVaheneeOikeinKunRahaaOnTarpeeksi() {
+      kortti.otaRahaa(9);
+      assertSaldo(1, kortti);
+    }
+
+    @Test
+    public void saldoEiVaheneKunRahaaEiOleTarpeeksi() {
+      kortti.otaRahaa(20);
+      assertSaldo(10, kortti);
+    }
+
+    @Test
+    public void otaRahaaPalauttaaTrueKunRahaRiittaa() {
+      assertTrue(kortti.otaRahaa(5));
+    }
+
+    @Test
+    public void otaRahaaPalauttaaFalseKunRahaEiRiita() {
+      assertFalse(kortti.otaRahaa(15));
     }
 }
